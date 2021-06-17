ddd20f92bd8354836d1c7400cfb8c15737c56646
dmelcher <danmelcher1@gmail.com>
2021-06-17 15:18:19 -0500

    Latest catchup to 16.4 mods

---
+++ Login HTML/innovate-financial-services-demo/js/index.js
@@ -0,0 +1,7 @@
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/3M1jVB5/card1.png" alt="card1" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/JvXGvFG/card2.png" alt="card2" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/Q9WD3Tj/card3.png" alt="card3" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/z4z18f1/card4.png" alt="card4" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/dDc5vs2/cardico.png" alt="cardico" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/6Py2P9d/cardico2.png" alt="cardico2" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/pvhGSP1/inno.png" alt="inno" border="0"></a><br /><a target='_blank' href='https://freeonlinedice.com/'>jenga online unblocked</a><br />
